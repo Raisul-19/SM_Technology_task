4cf64e9939d83de93bad37fa2d87b873786d9309
Raisul-19 <raisul15-14119@diu.edu.bd>
2025-04-16 22:13:27 +0600

    SM Technology Task

---
+++ main.js
@@ -0,0 +1,40 @@
+// slider page move on
+
+const slider = document.getElementById('slider');
+
+function slideLeft() {
+  slider.scrollLeft -= 270;
+}
+
+function slideRight() {
+  slider.scrollLeft += 270;
+}
+
+
+
+// query page details view
+
+const items = document.querySelectorAll('.query-item');
+
+    items.forEach(item => {
+      const header = item.querySelector('.query-header');
+
+      header.addEventListener('click', () => {
+        const openItem = document.querySelector('.query-item.active');
+        if (openItem && openItem !== item) {
+          openItem.classList.remove('active');
+          toggleIcon(openItem, false);
+        }
+
+        item.classList.toggle('active');
+        toggleIcon(item, item.classList.contains('active'));
+      });
+    });
+
+    function toggleIcon(item, isOpen) {
+      const icon = item.querySelector('.query-icon');
+      icon.className = isOpen 
+        ? 'fa-solid fa-circle-minus query-icon' 
+        : 'fa-solid fa-circle-plus query-icon';
+    }
+
